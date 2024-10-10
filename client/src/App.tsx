fcf2c71d09663b59c5b8adf83f253b59ff7e22cb
Catherine Zappia <catzappia@aol.com>
2024-10-09 20:40:22 -0400

    I don't want to see any stock files ever again.
    Can't believe I made this mistake but oh well.
    I really hope this works.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,36 @@
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import FinancialNews from './Components/News'
+import SavedStocks from './Pages/SavedStocks'
 
-import  {Footer}  from './Componets/Footer/Footer'
-import Header from './Componets/Header/Header'
-import Chart from './Pages/Chart'
-import Home from './Pages/HomePage/Home'
-// import Chart from './Pages/chart'
-
-
-
-
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 
 
 
 
-function App() {
-  
 
+
+
+
+
+const App: React.FC = () => {
   return (
-    <>
-    <Header/>
-     <Home/>
-     <Footer/>
-    <Chart/>
+    <Router>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/news" element={<FinancialNews />} />
+      <Route path="/saved-stocks" element={<SavedStocks />} />
+    </Routes>
 
-      
-    
-    </>
+     <Footer />
+    </Router>
   )
 }
 
-export default App
+export default App;
