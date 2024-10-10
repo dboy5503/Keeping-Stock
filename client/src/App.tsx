c2bb64fdf0dd9741c1a9fa9bf528d2da4cc02248
Catherine Zappia <catzappia@aol.com>
2024-10-10 19:45:09 -0400

    removed html files, moved page files from components to Pages folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' // needs to be installed
-import Home from './Components/HomePage/Home'
-import Login from './Components/Login/Login'
-import FinancialNews from './Components/News/News'
-import SavedStocks from './Components/SavedStocks/Saved-Stocks'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import FinancialNews from './Pages/News'
+import SavedStocks from './Components/EditableTitle/Saved-Stocks'
 
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
