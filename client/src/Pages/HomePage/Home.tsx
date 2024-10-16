4e22dbdc0d1cfe8732570fbbc16b3f1f8403bf44
Catherine Zappia <catzappia@aol.com>
2024-10-15 21:57:17 -0400

    ok, went through and fixed home and news. i don't think i was able to locate the red/yellow/green overlays

--- client/src/Pages/HomePage/Home.tsx
+++ client/src/Pages/HomePage/Home.tsx
@@ -3,77 +3,26 @@ import CardGroup from 'react-bootstrap/CardGroup';
 import './Home.css';
 import LineChart, { LineChart2, LineChart3 } from '../../Components/Chart/LineChart';
 
-
-
-
-
-
-
-    
 export default function Home() {
-  
-
-  
-  
-
-
-
-
-
-   
-            return (
-              <div>
-            
-              <CardGroup className='container'>
-                
-                <Card className='box-card'>
-                  
-                  
-                  <Card.Body>
-                    <Card.Title className='title'>AAPL </Card.Title>
-                    <Card.Text>
-                      Description 
-                    </Card.Text>
-                   
-                  </Card.Body>
-                  <Card.Footer>
-                  <small className="text-muted">Market Cap </small>
-                  </Card.Footer>
-                  <LineChart/>
-                  
-                </Card>
-                
-                <Card className='box-card'>
-                 
-                  <Card.Body>
-                    <Card.Title className='title'>META</Card.Title>
-                    <Card.Text>
-                      Description 
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Footer>
-                    <small className="text-muted">Stock data on Chart </small>
-                  </Card.Footer>
-                  <LineChart2/>
-                </Card>
-                <Card className='box-card'>
-                  
-                  <Card.Body>
-                    <Card.Title className='title'>NVDA</Card.Title>
-                    <Card.Text>
-                      Description 
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Footer>
-                  <small className="text-muted">Stock data on Chart </small>
-                  </Card.Footer>
-                  <LineChart3/>
-                </Card>
-              </CardGroup>
-              </div>
-
-            );
-          }
+  return (
+    <div>
+      <CardGroup className='container'>
+        <Card className='box-card'>
+          <Card.Title className='title'>AAPL</Card.Title>
+          <LineChart />
+        </Card>
+        <Card className='box-card'>
+          <Card.Title className='title'>META</Card.Title>
+          <LineChart2 />
+        </Card>
+        <Card className='box-card'>
+          <Card.Title className='title'>NVDA</Card.Title>
+          <LineChart3 />
+        </Card>
+      </CardGroup>
+    </div>
+  );
+}
           
        
     
